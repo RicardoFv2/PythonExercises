d88305ddcc3e6487a2bf00becee25b22e2517041
Ricardo Fuentes <ricardisimofuentes@hotmail.com>
2023-07-24 17:54:49 -0600

    Tercer ejercicio

---
+++ ejercicios_guia3.py
@@ -0,0 +1,20 @@
+# Desarrolle un programa que solicite al usuario ingresar dos números. Luego, declare una función
+# para dividir el primer número entre el segundo número y muestra el resultado. Si el segundo
+# número es cero, maneja el error de división por cero y muestra un mensaje adecuado.
+
+# try:
+#     numero1 = int(input("Ingrese el numero 1: "))
+#     numero2 = int(input("Ingrese el numero 2: "))
+
+#     def dividir_numeros(num1, num2):
+#         return (numero1 / numero2)
+
+#     print(f"El resultado de la división entre {numero1} y {numero2} es: {dividir_numeros(numero1,numero2)}")
+# except ValueError:
+#     print("Ingrese un numero valido, no se aceptan caracteres")
+  
+# except ZeroDivisionError:
+#     print("No se puede dividir entre 0")
+
+
+  
